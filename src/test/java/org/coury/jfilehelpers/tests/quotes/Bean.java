1a8042c54582de5092feae61ed0f01b1a7359b2c
dstarh <dstarh@purpleasphalt.com>
2011-08-12 10:04:46 -0400

    more tests for quotes

--- src/test/java/org/coury/jfilehelpers/tests/quotes/Bean.java
+++ src/test/java/org/coury/jfilehelpers/tests/quotes/Bean.java
@@ -3,7 +3,7 @@ package org.coury.jfilehelpers.tests.quotes;
 import org.coury.jfilehelpers.annotations.DelimitedRecord;
 import org.coury.jfilehelpers.enums.QuoteMode;
 
-@DelimitedRecord(value="|", quoteChar='\'', quoteMode=QuoteMode.OptionalForBoth)
+@DelimitedRecord(value="|", quoteChar='"', quoteMode=QuoteMode.OptionalForBoth)
 public class Bean {
 	private String firstName;
 
--- src/test/java/org/coury/jfilehelpers/tests/quotes/TestQuotes.java
+++ src/test/java/org/coury/jfilehelpers/tests/quotes/TestQuotes.java
@@ -1,6 +1,7 @@
 package org.coury.jfilehelpers.tests.quotes;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -9,25 +10,72 @@ import junit.framework.Assert;
 import org.coury.jfilehelpers.engines.FileHelperEngine;
 import org.junit.Test;
 
-
 public class TestQuotes {
 
 	@Test
-	public void testQuotes(){
+	public void testWithQuotes() throws IOException {
 		StringBuilder s = new StringBuilder();
-		s.append("'david'|'herman'");
+		s.append("\"david\"|\"herman\"");
 		ByteArrayInputStream bais = new ByteArrayInputStream(s.toString().getBytes());
 		InputStreamReader r = new InputStreamReader(bais);
 		FileHelperEngine<Bean> e = new FileHelperEngine<Bean>(Bean.class);
+
+		List<Bean> beans = e.readStream(r, -1);
+		for (Bean b : beans) {
+			Assert.assertEquals("david", b.getFirstName());
+		}
+
+	}
+
+	@Test
+	public void testWithoutQuotes() throws IOException {
+		StringBuilder s = new StringBuilder();
+		s.append("david|herman");
+		ByteArrayInputStream bais = new ByteArrayInputStream(s.toString().getBytes());
+		InputStreamReader r = new InputStreamReader(bais);
+		FileHelperEngine<Bean> e = new FileHelperEngine<Bean>(Bean.class);
+
+		List<Bean> beans = e.readStream(r, -1);
+		for (Bean b : beans) {
+			Assert.assertEquals("david", b.getFirstName());
+		}
+
+	}
+
+	@Test
+	public void testWithQuotesAndNonEscapedDelimiter() throws IOException {
+		StringBuilder s = new StringBuilder();
+		s.append("\"dav|id\"|\"herman\"");
+		ByteArrayInputStream bais = new ByteArrayInputStream(s.toString().getBytes());
+		InputStreamReader r = new InputStreamReader(bais);
+		FileHelperEngine<Bean> e = new FileHelperEngine<Bean>(Bean.class);
+
 		try {
 			List<Bean> beans = e.readStream(r, -1);
 			for (Bean b : beans) {
-				Assert.assertEquals("david", b.getFirstName());
+				Assert.assertEquals("dav|id", b.getFirstName());
 			}
 		}
-		catch (Throwable t) {
-			t.printStackTrace();
+		catch (Throwable e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+
+	}
+
+	@Test
+	public void testWithQuotesAndNonEscapedDelimiterAndExtraQuote() throws IOException {
+		StringBuilder s = new StringBuilder();
+		s.append("\"da\"\"vid\"|\"herman\"");
+		ByteArrayInputStream bais = new ByteArrayInputStream(s.toString().getBytes());
+		InputStreamReader r = new InputStreamReader(bais);
+		FileHelperEngine<Bean> e = new FileHelperEngine<Bean>(Bean.class);
+
+		List<Bean> beans = e.readStream(r, -1);
+		for (Bean b : beans) {
+			Assert.assertEquals("da\"vid", b.getFirstName());
+		}
+
 	}
 
 }
